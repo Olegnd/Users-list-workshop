6237fb53e67b26332bcde9d8dae3388d739500df
Olegnd <olegnd63@gmail.com>
2016-10-26 00:25:55 +0300

    update

--- src/create.js
+++ src/create.js
@@ -1,7 +1,29 @@
 export class CreatePage {
 
 }
-
 export default function initPage() {
-	alert('create page')
+	//alert('create page')
+    document.querySelector('#create-page').addEventListener('submit', function() {
+        event.preventDefault();
+    })
+    var name = document.querySelector('.name').value;
+    var password = document.querySelector('.password').value;
+    var email = document.querySelector('.email').value;
+    var comment = document.querySelector('.comment').value;
+    var payload = {
+        name: name,
+        password: password,
+        email: email,
+        comment: comment
+        };
+
+var data = new FormData();
+data.append( "json", JSON.stringify( payload ) );
+
+fetch("http://193.111.63.76:3000/api/v1/Users", {
+    method: "POST",
+    body: data
+}).then(function(res){ return res.json(); })
+  .then(function(data){ alert( JSON.stringify(data)); })
+
 }
