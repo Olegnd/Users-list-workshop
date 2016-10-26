1fe238d4aa8005e65d0284fbd581645fa5f1ff2c
Olegnd <olegnd63@gmail.com>
2016-10-26 11:28:48 +0300

    update

--- src/create.js
+++ src/create.js
@@ -2,33 +2,33 @@ export class CreatePage {
 
 }
 export default function initPage() {
-	//alert('create page')
-    document.querySelector('#create-page').addEventListener('submit', function() {
+    //alert('create page')
+    document.querySelector('#create-page').addEventListener('submit', function () {
         event.preventDefault();
-        
-    var name = document.querySelector('.id').value;
-    var name = document.querySelector('.name').value;
-    var password = document.querySelector('.password').value;
-    var email = document.querySelector('.email').value;
-    var comment = document.querySelector('.comment').value;
-    var submit = document.querySelector('.submit').value;
-    var payload = {
-        id: id,
-        name: name,
-        password: password,
-        email: email,
-        comment: comment
+
+        var id = document.querySelector('.id').value;
+        var name = document.querySelector('.name').value;
+        var password = document.querySelector('.password').value;
+        var email = document.querySelector('.email').value;
+        var comment = document.querySelector('.comment').value;
+        var submit = document.querySelector('.submit').value;
+        var payload = {
+            id: id,
+            name: name,
+            password: password,
+            email: email,
+            comment: comment
         };
 
-var data = new FormData();
-data.append( "json", JSON.stringify( payload ) );
+        var data = new FormData();
+        data.append("json", JSON.stringify(payload));
 
-fetch("http://193.111.63.76:3000/api/v1/Users", {
-    method: "POST",
-    body: data
-}).then(function(res){ return res.json(); })
-  .then(function(data){ alert( JSON.stringify(data)); })
+        fetch("http://193.111.63.76:3000/api/v1/Users", {
+            method: "POST",
+            body: data
+        }).then(function (res) { return res.json(); })
+            .then(function (data) { alert(JSON.stringify(data)); })
 
     });
-    
+
 }
--- src/list.js
+++ src/list.js
@@ -4,54 +4,54 @@ export class ListPage {
 
 export default function initPage() {
 	//alert('list page')
-	fetch('http://193.111.63.76:3000/api/v1/Users')  
-  .then(  
-    function(response) {  
-      if (response.status !== 200) {  
-        console.log('Looks like there was a problem. Status Code: ' +  
-          response.status);  
-        return;  
-      }
+	fetch('http://193.111.63.76:3000/api/v1/Users')
+		.then(
+		function (response) {
+			if (response.status !== 200) {
+				console.log('Looks like there was a problem. Status Code: ' +
+					response.status);
+				return;
+			}
 
-      // Examine the text in the response  
-      response.json().then(function(data) {  
-        console.log(data);
-		var index = 0;
-        var i;
-        var arrayKey = [];
-		var main  = document.querySelector('main');
-        var table = document.querySelector('table');
-        var thead = document.querySelector('thead');
-        var tbody = document.querySelector('tbody');
-        var tr;
-        var arrayTrTbody = [];
-        var th = [];
-        var td = [];
-        arrayKey = Object.keys(data[0]);
+			// Examine the text in the response  
+			response.json().then(function (data) {
+				console.log(data);
+				var index = 0;
+				var i;
+				var arrayKey = [];
+				var main = document.querySelector('main');
+				var table = document.querySelector('table');
+				var thead = document.querySelector('thead');
+				var tbody = document.querySelector('tbody');
+				var tr;
+				var arrayTrTbody = [];
+				var th = [];
+				var td = [];
+				arrayKey = Object.keys(data[0]);
 
-        //--form HTML document--
-        tr = document.createElement('tr');
-        for (index; index < arrayKey.length - 1; index++) {
-            th[index] = '<th class="' + arrayKey[index] + '">' + arrayKey[index] + '</th>';
-        }
-        thead.innerHTML = '<tr>' + th.join('') + '</tr>';
+				//--form HTML document--
+				tr = document.createElement('tr');
+				for (index; index < arrayKey.length - 1; index++) {
+					th[index] = '<th class="' + arrayKey[index] + '">' + arrayKey[index] + '</th>';
+				}
+				thead.innerHTML = '<tr>' + th.join('') + '</tr>';
 
-        index = 0;
-        for (index; index < data.length; index++) {
-            tr = document.createElement('tr');
-            i = 0;
-            for (i; i < arrayKey.length - 1; i++) {
-                td[i] = '<td>' + data[index][arrayKey[i]] + '</td>';
-            }
-            arrayTrTbody[index] = '<tr>' + td.join('') + '</tr>';
-            tr.innerHTML = arrayTrTbody[index];
-            tbody.appendChild(tr);
-        }  
-      });  
-    }  
-  )  
-  .catch(function(err) {  
-    console.log('Fetch Error :-S', err);  
-  });
+				index = 0;
+				for (index; index < data.length; index++) {
+					tr = document.createElement('tr');
+					i = 0;
+					for (i; i < arrayKey.length - 1; i++) {
+						td[i] = '<td>' + data[index][arrayKey[i]] + '</td>';
+					}
+					arrayTrTbody[index] = '<tr>' + td.join('') + '</tr>';
+					tr.innerHTML = arrayTrTbody[index];
+					tbody.appendChild(tr);
+				}
+			});
+		}
+		)
+		.catch(function (err) {
+			console.log('Fetch Error :-S', err);
+		});
 
 }
